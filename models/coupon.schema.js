d1acc52d652934948fc0f23ca0745a850852cf10
Ved Dadhich <111679327+Veddadhich1997@users.noreply.github.com>
2023-01-06 14:18:14 +0000

    coupon schema added

---
+++ models/coupon.schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+    {
+        code:{
+            type: String,
+            required: [true,"Please provide a coupon name"] 
+        },
+
+        discount:{
+            type: Number,
+            default: 0
+        },
+
+        active:{
+            type: Boolean,
+            default: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+export default mongoose.Model("Coupon", couponSchema);
